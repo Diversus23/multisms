c84156caf645465bdf0d929d499d8d0698d06858
Diversus23 <support@softonit.ru>
2025-02-07 14:16:16 +0300

    feat: Обновлен файл для сборки дистрибутива

--- make.cmd
+++ make.cmd
@@ -11,4 +11,6 @@ set version=1.0.0.54
 set source=".\*.epf" ".\doc\*.pdf"
 set destination="."
 
+oscript "c:\Projects\actions\src\actions.os" changelog convert --in ".\CHANGELOG.md" --out "%version%.html" --template "SimpleWithoutVersion.html" --productname "$CI_PROJECT_TITLE" --version "%version%" --minifyhtml --parser_configuration_src "c:\Projects\it\it"
+
 "C:\Program Files\7-Zip\7z.exe" a -tzip -ssw -mx9 %destination%\multisms1c_%version%.zip %source%
